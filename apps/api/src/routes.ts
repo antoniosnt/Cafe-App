89d64800fb33ab820e876f1778857345479ae060
antoniosnt <antoniosnt.dev@gmail.com>
2026-06-12 10:08:22 -0300

    feat: adjust routes, add .env.example and dotenv

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -2,8 +2,16 @@ import { Request, Response, Router } from "express";
 
 const applicationRoutes = Router();
 
-applicationRoutes.use("/healthy", (_: Request, res: Response) => {
+applicationRoutes.get("/", (_: Request, res: Response) => {
+	return res.status(200).send("<h1>CAFE APP API V1</h1>");
+});
+
+applicationRoutes.get("/healthy", (_: Request, res: Response) => {
 	return res.status(200).send({ status: "healthy" });
+});
+
+applicationRoutes.use((_req: Request, res: Response) => {
+	return res.status(404).send({ message: "not found" });
 });
 
 export default applicationRoutes;
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,10 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
 
+// Dotenv
+import dotenv from "dotenv";
+
+dotenv.config();
+
 // Routes
 import applicationRoutes from "@api/src/routes";
 
 const app = express();
 
+const HOST = process.env.API_HOST || "localhost";
 const PORT = process.env.API_PORT || 3001;
 const ENVIRONMENT = process.env.ENVIRONMENT || "HOMOLOGACAO";
 
@@ -17,5 +23,9 @@ app.use((req: Request, _: Response, next: NextFunction) => {
 app.use("/api/v1", applicationRoutes);
 
 app.listen(PORT, () => {
-	console.log(`Listening Server on PORT ${PORT} -- ${ENVIRONMENT}`);
+	if (ENVIRONMENT === "HOMOLOGACAO") {
+		console.log(
+			`Listening Server on http://${HOST}:${PORT}/api/v1 -- ${ENVIRONMENT}`,
+		);
+	}
 });
