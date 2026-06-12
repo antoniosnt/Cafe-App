4850c3909f168986a1a81690647d3caf966c62b6
antoniosnt <antoniosnt.dev@gmail.com>
2026-06-12 11:21:21 -0300

    feat: add dockerfile, docker-compose and database connection

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,10 +3,13 @@ import express, { NextFunction, Request, Response } from "express";
 // Dotenv
 import dotenv from "dotenv";
 
-dotenv.config();
-
 // Routes
-import applicationRoutes from "@api/src/routes";
+import applicationRoutes from "@api/routes";
+
+// Database
+import connection from "@api/config/connections/database";
+
+dotenv.config();
 
 const app = express();
 
@@ -22,10 +25,27 @@ app.use((req: Request, _: Response, next: NextFunction) => {
 
 app.use("/api/v1", applicationRoutes);
 
-app.listen(PORT, () => {
-	if (ENVIRONMENT === "HOMOLOGACAO") {
-		console.log(
-			`Listening Server on http://${HOST}:${PORT}/api/v1 -- ${ENVIRONMENT}`,
-		);
+class ApplicationServer {
+	public async execute() {
+		try {
+			await connection.connect();
+
+			if (ENVIRONMENT === "HOMOLOGACAO") {
+				const result = await connection.query("SELECT VERSION()");
+				console.log(result.rows[0].version);
+			}
+
+			app.listen(PORT, () => {
+				console.log(
+					`Listening Server on http://${HOST}:${PORT}/api/v1 -- ${ENVIRONMENT}`,
+				);
+			});
+		} catch (error) {
+			console.error("Startup error:", error);
+			process.exit(1);
+		}
 	}
-});
+}
+
+const server = new ApplicationServer();
+server.execute();
